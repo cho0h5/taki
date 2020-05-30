a268ecfad2c94989600ed28d3c32e7c4de566c46
Youngho Cho <dominoyh5@gmail.com>
2020-05-30 16:57:48 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
-package taki
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+const (
+	Server = 1 + iota
+	Client
+	Exit
+)
 
 func main() {
-	fmt.Println("Hi");
+	fmt.Println("Welcome to TAKI\n")
+	fmt.Println("1. Server")
+	fmt.Println("2. Client")
+	fmt.Println("3. Exit\n")
+	fmt.Print("Enter: ")
+
+	for {
+		var mode int
+		fmt.Scan(&mode)
+
+		switch mode {
+		case Server:
+			StartServer()
+			os.Exit(0)
+		case Client:
+			StartClient()
+			os.Exit(0)
+		case Exit:
+			os.Exit(0)
+		}
+
+		fmt.Print("\nEnter again: ")
+	}
 }
